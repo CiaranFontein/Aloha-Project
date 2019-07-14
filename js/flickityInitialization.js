d50aa98d9723820fab86a094855e96990c753fc7
Ciaran Fontein <ciaranfontein@gmail.com>
2019-07-14 15:20:11 -0700

    Html language set to english

--- js/flickityInitialization.js
+++ js/flickityInitialization.js
@@ -1,7 +1,6 @@
 function ready() {
   const elem = document.querySelector('.main-carousel');
   const flkty = new Flickity(elem, {
-    // options
     cellAlign: 'left',
     autoPlay: true,
     wrapAround: true,
