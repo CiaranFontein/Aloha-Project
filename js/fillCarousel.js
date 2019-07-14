cf24b9ace3d4d6899554966842e0db2373ba4e4f
Ciaran Fontein <ciaranfontein@gmail.com>
2019-07-14 15:31:26 -0700

    🗿Added an alt attribute to imgs  👐

--- js/fillCarousel.js
+++ js/fillCarousel.js
@@ -9,14 +9,14 @@ class Product {
 const mainCarousel = document.getElementsByClassName("main-carousel")[0];
 
 products = [
-    new Product("Dresses", "Stretch Knit Dress", "$169.00", "images/stretch-knit-dress.jpg"),
-    new Product("Shirts", "Cotton Blue Shirt", "$89.00", "images/cotton-blue-shirt.jpg"),
-    new Product("Dresses", "Checked Stretch Dress", "$129.00", "images/checked-stretch-dress.jpg"),
-    new Product("Bags", "Blue Hipster Backpack", "$69.00", "images/blue-hipster-backpack.jpg"),
-    new Product("Denim", "Denim Overall Shorts", "$89.00", "images/demin-overall-shorts.jpg"),
-    new Product("Dresses", "Pink Dotted Dress", "$99.00", "images/pink-dotted-dress.jpg"),
-    new Product("Dresses", "Blue Silk Dress", "$119.00", "images/blue-silk-dress.jpg"),
-    new Product("Shirts", "Cotton Yellow Tshirt", "$39.00", "images/cotton-yellow-tshirt.jpg")
+    new Product("Dresses", "Stretch Knit Dress", "$169.00", "images/stretch-knit-dress.jpg", "stretch knit dress"),
+    new Product("Shirts", "Cotton Blue Shirt", "$89.00", "images/cotton-blue-shirt.jpg", "cotton blue shirt"),
+    new Product("Dresses", "Checked Stretch Dress", "$129.00", "images/checked-stretch-dress.jpg", "checked stretch dress"),
+    new Product("Bags", "Blue Hipster Backpack", "$69.00", "images/blue-hipster-backpack.jpg", "blue hipster backpack"),
+    new Product("Denim", "Denim Overall Shorts", "$89.00", "images/demin-overall-shorts.jpg", "denim overall shorts"),
+    new Product("Dresses", "Pink Dotted Dress", "$99.00", "images/pink-dotted-dress.jpg", "pink dotted dress"),
+    new Product("Dresses", "Blue Silk Dress", "$119.00", "images/blue-silk-dress.jpg", "blue silk dress"),
+    new Product("Shirts", "Cotton Yellow Tshirt", "$39.00", "images/cotton-yellow-tshirt.jpg", "cotton yellow t-shirt")
 ];
 
 //Create a carousel-cell for each Product in products
@@ -26,6 +26,7 @@ products.forEach(function (product, index) {
 
     let newCarouselImg = document.createElement('img');
     newCarouselImg.setAttribute("src", products[index].imageSource);
+    newCarouselImg.setAttribute("alt", products[index].imageAlt);
     newCarouselCell.append(newCarouselImg);
 
     let newCarouselCategory = document.createElement('p');
